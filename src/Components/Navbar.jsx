886c4db6ac1d71d8a8c524e4e3daf967f305515b
Sai Kiran Boddu <bsaikiran618@gmail.com>
2022-03-06 23:56:17 +0530

    Revert "about us kadu, just about"
    This reverts commit 9035b375c4acab5795290227d6880aaa12586ba4.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,16 +4,10 @@ import React from "react";
 export default function Navbar() {
   return (
     <div className="flex flex-col sm:flex-row bg-[#121212]  w-full items-center gap-4 sm:justify-between md:px-[6rem] px-[2rem] py-4">
-      <span className="text-[1.25rem] md:text-[1.75rem] text-[#e1e1e1] font-mono text-center">
-        <span className="text-[#16e2f3]">import</span> <span>initialize</span>{" "}
-        <span className="text-[#16e2f3]">from</span> cS
-        <span className="text-[#9333ea]">.events</span>
-      </span>
+      <span className="text-[1.25rem] md:text-[1.75rem] text-[#e1e1e1] font-mono text-center"><span className="text-[#16e2f3]">import</span> <span >initialize</span> <span className="text-[#16e2f3]">from</span> cS<span className="text-[#9333ea]">.events</span></span>
       <div>
         <a target="_blank" href="https://www.codingstudio.club/">
-          <span className="text-[#e1e1e1] cursor-pointer mr-8 text-[1.1rem]">
-            About
-          </span>
+          <span className="text-[#e1e1e1] cursor-pointer mr-8 text-[1.1rem]">About Us</span>
         </a>
         <a href="#form">
           <Button
