00fef8aa6e8b12a0bd293346bbcbe383b72514e7
Pranchal15 <pranchal.agarwal@gmail.com>
2022-03-06 20:09:16 +0530

    UI changes

--- src/Components/AboutEvent.jsx
+++ src/Components/AboutEvent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AboutEvent() {
   return (
     <div className="relative shadow-xl min-h-[30rem] md:w-[95%]  lg:w-[90%] xl:w-[65%]  w-[90%] mx-auto md:ml-auto md:mr-0   bg-[#1e1e1e]  p-8">
-      <span className="text-[2.2rem]">About <span className="font-mono">cS.</span><em><span className="text-[#9333ea] font-mono" >initialize</span><span className="font-mono">();</span></em></span>
+      <span className="text-[2.2rem]">About <em><span className="text-[#9333ea] font-mono" >initialize</span><span className="font-mono">();</span></em></span>
       <div className="mt-2">
         <span>
         Initialize is a 5-weeks event focused on competitive programming, data structures and algorithms. During the course of five weeks, you will be learning various DSA, and Competitive Programming concepts right from the basics and also be participating in exciting contests every week. The objective of Initialize is to create a culture of consistent competitive programming practice and also encourage peers who want to kick-start their competitive programming journey. Our goal is to give you a taste of the adrenaline rush we get when we solve problems.
