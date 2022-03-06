da0d09341611e33589ba5d2110eee63c2cd4337d
Pranchal15 <pranchal.agarwal@gmail.com>
2022-03-06 20:43:13 +0530

    expose the server

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import svgr from '@honkhonk/vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(),react()]
+  plugins: [svgr(),react()],
+  server: {
+    host: true
+  }
 })
